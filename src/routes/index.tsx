8a1f64153174ec0560a9c76a98591650c1359f0a
jorgin <jorgeigor123@hotmail.com>
2025-08-20 11:24:31 -0300

    feat: Iniciando Homepage e Layout de paginas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter } from "react-router";
 
 import { AuthRoutes } from "./AuthRoutes.tsx";
+import { HomePage } from "./HomePage.tsx";
 export function Routes(){
     return(
         <BrowserRouter>
-            <AuthRoutes />
+            <HomePage />
         </BrowserRouter>
     )
 }
