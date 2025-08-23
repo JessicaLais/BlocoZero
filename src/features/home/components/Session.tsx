a59b82bb822afb276e84d8b28a1ffd622a04b992
jorgin <jorgeigor123@hotmail.com>
2025-08-22 23:46:45 -0300

    fix: corrigindo estilização do componente Session

--- src/features/home/components/Session.tsx
+++ src/features/home/components/Session.tsx
@@ -15,7 +15,7 @@ export function SessionItem({ data, ...rest }: Props) {
     console.log(data)   
     return (
         <a 
-            className="flex items-center gap-3 hover:bg-green-100/5 cursor-pointer rounded-md p-2"
+            className="flex items-center gap-3 hover:bg-green-100/5 cursor-pointer rounded-md border-1 border-gray-300 p-2"
             {...rest}
         >
             <img className="w-8 h-8" src={data.photoUrl} alt="Ícone da categoria"/>
